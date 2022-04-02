ea7c50a97cb5d268f7a6e7d79468632d9daac6d6
ruben <rubendasilva@me.com>
2022-04-02 12:38:32 -0700

    "cells, button, and message area added"

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git add -A
+echo "Enter commit message (add punctuations): "
+read cmtMsg
+git commit -m "$cmtMsg"
+echo "Enter branch to push (master or main)?"
+read pshBrnch
+git push -u origin $pshBrnch
+echo "<><><>"
+echo
+echo "Push complete :) "
+echo
+echo "<><><>"
